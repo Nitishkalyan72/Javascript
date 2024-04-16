2040cf86777c9e4726bb9cf56de76e7881945901
Nitishkalyan72 <93877182+Nitishkalyan72@users.noreply.github.com>
2024-04-16 11:24:08 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"//treat all js code as newer version
+
+//alert(3+3)// we are using node.js not browser
+
+let str = "Ram"
+//PRIMITIVE VALUES
+/*number => 2 to the power 53
+BigInt
+String
+Boolean
+null - standalone value 
+undefined => Means value assign nhi hue hai
+symbol - for uniqueness*/
+
+console.log(typeof str)
+console.log(typeof null) // type of null is "object" (kch logo ka khna hai ki yae bhot bdi mistake hai)
+console.log(typeof undefined) //=> undefined
+
