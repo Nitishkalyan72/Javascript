3311b2d46352c2846799f72c671a2f6ae4735d06
Nitishkalyan72 <93877182+Nitishkalyan72@users.noreply.github.com>
2024-04-17 13:10:17 +0000

    learning

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 //==============================Data type conversion confusion clear krdega ==========================
 
 
-// let score = "33" //after conversion in number .it will give correct output
-// let score = "33aad"//after conversion in number will give NaN
-// let score = null //after conversion into number will convert into zero
-// let score = undefined //after conversion in number will give NaN
-let score = true //after conversion will convert into 1 if false then it will convert into 0
+// let score = "33" //after conversion in number .it will give correct output 33 => 33
+// let score = "33aad"//after conversion in number will give NaN (33aad => NaN)
+// let score = null //after conversion into number will convert into zero (null => 0)
+// let score = undefined //after conversion in number will give NaN (undefined  => NaN)
+let score = true //after conversion will convert into 1 if false then it will convert into 0 (true => 1)
 console.log(typeof score)
 let newScore = Number(score)
 console.log(typeof newScore)
@@ -17,10 +17,45 @@ console.log(newScore)
 let isLoggedIn = "rk"
 let check = Boolean(isLoggedIn)
 console.log(check)
-
 //1=>true
 //0=>false
 // rk(string mae koi b value hogi) => true
 // "" => false
+
+
+
+
+
+
+//========================OPERATIONS===========================
+
+let val = 3
+let newVal = -val
+console.log(newVal) // -3
+
+
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3) // 2 ki power 3
+console.log(2/2)
+console.log(5%2)  //1
+
+
+console.log("2"+"2") // 22
+console.log("2"+2+2) // 222
+console.log(2+2+"2") // 42
+
+
+//=============prefix vs postfix====================
+let x = 3
+let y = x++
+console.log(x +" , " + y)
+
+let a = 3
+let b = ++a
+console.log(a +" , "+ b)
+
 
 
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+console.log("2">1)
+console.log("02"<1)
+
+// Inconsistency hai language mai (iss tarha ka code hmesa avoid kro)
+/*console.log(null>0)
+console.log(null>=0) //=> true
+console.log(null == 0)
+
+console.log(undefined >0)
+console.log(undefined <0)
+console.log(undefined == 0)*/
+
+//Strictly check
+console.log("2"===2)     //=>false
