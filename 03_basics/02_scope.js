1700d6831682b1866b25a3cc677d0bc747578328
Nitishkalyan72 <93877182+Nitishkalyan72@users.noreply.github.com>
2024-06-10 15:28:18 +0000

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -13,3 +13,45 @@ console.log(c)//31
 
 
 //IMP - Browser kae anddr jake jb hm js run krte hai vha global scope alg hota hai or jb hmm node environment mae system mae run krte hai vha pae global scope alg hai
+function one(){
+    const userName = "ram";
+    function two(){
+        const website = "youtubbe"
+        console.log(userName);
+    }
+    //console.log(website)//ReferenceError: website is not defined
+    two();
+}
+one()
+
+if(true){
+    const userName = "hitesh"
+    if(userName === "hitesh"){
+        const website = " Youtube"
+        console.log(userName+website)//hitesh Youtube
+    }
+}
+
+//TWO SYNTESES TO DECLARE A FUNCTION 
+//METHOD 1
+function addone(num){
+    return num+1
+}
+console.log(addone(5))
+
+//addTwo(5)//ReferenceError: Cannot access 'addTwo' before initialization
+//variables are very powerful in javascript 
+//declare krne kae bad hmm isse hold b krrre hai 
+//METHOD 2
+const addTwo = function(num){
+    return num+2
+}
+addTwo(5)
+console.log(addTwo)
+
+
+//THIS AND ARROW FUNCTION
+
+//IMPORTANT QUES - console.log(this) browser kae andr jo glabal objevt hai vo hai window object 
+console.log(this)//and in the node environment it is empty object
+//and this refers t current object 
