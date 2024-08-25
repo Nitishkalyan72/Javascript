ac2612f03c3ecd969e540eb221825debf229a4e6
Nitishkalyan72 <93877182+Nitishkalyan72@users.noreply.github.com>
2024-08-25 06:31:51 +0000

    change

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -3,7 +3,7 @@ console.log("02"<1)
 
 // Inconsistency hai language mai (iss tarha ka code hmesa avoid kro)
 /*console.log(null>0)
-console.log(null>=0) //=> true
+console.log(null>=0) //=> true //comparison convert null to a number and treating it as zero thatswhy null>=0 is true and null > 0 is equal to false
 console.log(null == 0)
 
 console.log(undefined >0)
